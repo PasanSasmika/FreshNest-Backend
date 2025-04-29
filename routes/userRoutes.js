2576b07f7f3db220ed76d406df0aeb4fc88bf2b9
PasanSasmika <pasansasmika198@gmail.com>
2025-04-29 20:07:05 +0530

    Γ£¿Get All users created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { creatAdmin, loginUser } from '../controllers/userController.js';
+import { creatAdmin, getAllUsers, loginUser } from '../controllers/userController.js';
 
 
 
 const userRouter = express.Router();
 
 userRouter.post("/",creatAdmin)
+userRouter.get("/",getAllUsers)
 userRouter.post("/login",loginUser)
 
 
