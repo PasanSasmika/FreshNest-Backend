3779476f3da26e1e38d725e4dccfbab616103584
PasanSasmika <pasansasmika198@gmail.com>
2025-04-29 08:43:02 +0530

    ≡ƒºæΓÇì≡ƒöºAdmin can update and delete services

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -30,3 +30,66 @@ export  function createService(req,res){
     })
 
 }
+
+
+// Get All Services
+
+export function getServices(req,res){
+    Service.find({}).then((services)=>{
+        res.json(services)
+    })
+}
+
+
+// Delete Service
+
+export function deleteServices(req,res){
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message: "Please login as an admin to delete services.!" 
+        })
+        return
+    }
+
+    const serviceId = req.params.serviceId
+
+    Service.deleteOne(
+        { _id: serviceId },
+    ).then(()=>{
+        res.json({
+            message: "Service deleted"
+        })
+    }).catch((error)=>{
+        res.status(403).json({
+            message: error
+        })
+    })
+
+}
+
+// Update Services
+
+export function updateServices(req,res){
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message: "Plese login as admin to update Services"
+        })
+        return
+    }
+
+    const serviceId = req.params.serviceId
+    const newServiceData = req.body
+
+    Service.updateOne(
+        { _id: serviceId },
+        newServiceData
+    ).then(()=>{
+        res.json({
+            message: "Service updated..!"
+        })
+    }).catch((error)=>{
+        res.status(500).json({
+            message: error
+        })
+    })
+}
