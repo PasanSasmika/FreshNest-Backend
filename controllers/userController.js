9520064311ef9d1842d355cee1e71cfda39816c9
PasanSasmika <pasansasmika198@gmail.com>
2025-04-29 07:26:42 +0530

    👤🛅 created fuctions for identifying the Users

--- controllers/userController.js
+++ controllers/userController.js
@@ -98,3 +98,31 @@ export function loginUser(req,res){
 }
 
 
+// Fuctions for identifying Users
+
+export function isAdmin(req){
+
+    if(req.user==null){
+        return false
+    }
+
+    if(req.user.type != "admin"){
+        return false
+    }
+
+    return true
+}
+
+export function isCustomer(req){
+
+    if(req.user==null){
+        return false
+    }
+
+    if(req.user.type != "customer"){
+        return false
+    }
+
+    return true
+
+}
