1ae01492ef9f96b18525d97ee43ee2045422c52f
PasanSasmika <pasansasmika198@gmail.com>
2025-04-29 07:02:28 +0530

    ✨only the admin can add admins

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,7 +14,21 @@ export function creatAdmin(req,res){
 const newUserData = req.body
 
 
+if(newUserData.type == "admin"){
 
+    if(req.user==null){
+        res.json({
+            message: "You are not loged in. plese log in as an admin to create admin account"
+        })
+        return
+    }
+
+    if(req.user.type !="admin"){
+        res.json({
+            message: "You are not an admin. plese log in as an admin"
+        })
+    }
+}
 
 
 
